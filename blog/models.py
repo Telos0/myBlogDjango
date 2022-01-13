cfd6bda7c6ee2b764205700278a6593ea892cb25
telos <my4357909@naver.com>
2022-01-13 15:24:31 +0000

    category add

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 import os
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -12,8 +22,12 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
@@ -23,3 +37,4 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
