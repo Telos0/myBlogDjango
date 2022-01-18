8d4800614232603abef17ec73bbae1497e192843
telos <my4357909@naver.com>
2022-01-18 13:21:34 +0000

    tags, post add add

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,18 @@ class Category(models.Model):
     def get_absolute_url(self):
         return f'/blog/category/{self.slug}'
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}'
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -25,6 +37,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Category, Tag
+from django.core.exceptions import PermissionDenied
+
+
+
 # Create your views here.
 
+# class views
 
 class PostList(ListView):
     model = Post
@@ -23,6 +29,33 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+# function views
+
 def category_page(request, slug):
     if slug == 'no_category':
         category = '미분류'
@@ -33,5 +66,14 @@ def category_page(request, slug):
 
     template_url = 'blog/post_list.html'
     context = { 'post_list' : post_list, 'categories' : Category.objects.all(), 'no_category_post_count' : Post.objects.filter(category=None).count(), 'category' : category, }
+
+    return render(request, template_url, context)
+
+def tag_page(request, slug):
+
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+    template_url = 'blog/post_list.html'
+    context = { 'post_list' : post_list, 'categories' : Category.objects.all(), 'no_category_post_count' : Post.objects.filter(category=None).count(), 'tag' : tag, }
 
     return render(request, template_url, context)
