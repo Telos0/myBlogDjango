3209060a9ae6580301a830ed5aae9f9a546c1736
telos <my4357909@naver.com>
2022-01-23 14:12:45 +0000

    paginate add, running services modify

--- single_pages/views.py
+++ single_pages/views.py
@@ -4,7 +4,7 @@ from blog.models import RunningServices
 # Create your views here.
 
 def landing(request):
-    running_services = RunningServices.objects.filter(runningYN=1)
+    running_services = RunningServices.objects.all()
     return render(request, 'single_pages/landing.html', { 'running_services' : running_services })
 
 def about_me(request):
