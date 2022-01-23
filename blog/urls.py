38a21cd497ec1b55e981da73568885ad515584bc
telos <my4357909@naver.com>
2022-01-23 13:52:52 +0000

    comment function add

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('tag/<str:slug>/', views.tag_page),
     path('create_post/', views.PostCreate.as_view()),
     path('update_post/<int:pk>', views.PostUpdate.as_view()),
+    path('<int:pk>/new_comment/', views.new_comment),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/', views.delete_comment),
+    path('delete_post/<int:pk>/', views.delete_post),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
-
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -27,6 +28,7 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
         return context
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -97,6 +99,16 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
                     tag.save()
                 self.object.tags.add(tag)
         return response
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
 # function views
 
 def category_page(request, slug):
@@ -120,3 +132,36 @@ def tag_page(request, slug):
     context = { 'post_list' : post_list, 'categories' : Category.objects.all(), 'no_category_post_count' : Post.objects.filter(category=None).count(), 'tag' : tag, }
 
     return render(request, template_url, context)
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.user.is_authenticated and request.user == post.author:
+        post.delete()
+        return redirect('/blog/')
+    else:
+        raise PermissionDenied
